c867f020f062f36f78620ac46d96b8a1f1ec1ceb
Dominic <owerwa@gmail.com>
2025-05-23 22:47:34 +0200

    ex00

--- module05/ex00/src/main.cpp
+++ module05/ex00/src/main.cpp
@@ -2,40 +2,45 @@
 
 int main(void) {
    
-   {
-    try {
-        Bureaucrat b("Bob", 0);
-    } catch (const Bureaucrat::GradeTooHighException&) {
-        std::cout << "Grade too high exception caught\n";
-    } catch (...) {
-        std::cout << "Unexpected exception type\n";
-    }
-   }
-   {
-    try {
-        Bureaucrat b("Bob", 151);
-    } catch (const Bureaucrat::GradeTooLowException&) {
-        std::cout << "Grade too low exception caught\n";
-    } catch (...) {
-        std::cout << "Unexpected exception type\n";
-    }
-   }
-   {
-    try {
-        Bureaucrat b("Dave", 1);
-        b.incrementGrade();
-    } catch (const std::exception& e) {
-        std::cout << "Caught: " << e.what() << "\n";
-    }
-   }
     {
-    try {
-        Bureaucrat b("Dave", 150);
-        b.decrementGrade();
-    } catch (const std::exception& e) {
-        std::cout << "Caught: " << e.what() << "\n";
+        try {
+            Bureaucrat b("Bob", 0);
+        } catch (const Bureaucrat::GradeTooHighException&) {
+            std::cout << "Grade too high exception caught\n";
+        } catch (...) {
+            std::cout << "Unexpected exception type\n";
+        }
     }
-   }
-   Bureaucrat b("Dave", 150);
-   std::cout << b << "\n";
+    std::cout << "\n";
+    {
+        try {
+            Bureaucrat b("Bob", 151);
+        } catch (const Bureaucrat::GradeTooLowException&) {
+            std::cout << "Grade too low exception caught\n";
+        } catch (...) {
+            std::cout << "Unexpected exception type\n";
+        }
+    }
+    std::cout << "\n";
+    {
+        try {
+            Bureaucrat b("Dave", 1);
+            b.incrementGrade();
+        } catch (const std::exception& e) {
+            std::cout << "Caught: " << e.what() << "\n";
+        }
+    }
+    std::cout << "\n";
+    {
+        try {
+            Bureaucrat b("Dave", 150);
+            b.decrementGrade();
+        } catch (const std::exception& e) {
+            std::cout << "Caught: " << e.what() << "\n";
+        }
+    }
+    std::cout << "\n";
+
+    Bureaucrat b("Dave", 150);
+    std::cout << b << "\n";
 }
