ac27111be67a21540607464bc653efd658de054b
Dominic <owerwa@gmail.com>
2025-07-12 00:38:50 +0200

    ex00

--- module08/ex00/include/easyfind.hpp
+++ module08/ex00/include/easyfind.hpp
@@ -4,3 +4,9 @@
 #include <stdexcept>
 
 template<typename T>
+typename T::iterator easyfind(T& container, int value) {
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::runtime_error("Value not found in container");
+    return it;
+}
